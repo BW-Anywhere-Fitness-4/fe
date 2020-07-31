f10e2e7bbba7cae0b28d9ece6a47512bc98e90e1
miahmo652 <60869648+miahmo652@users.noreply.github.com>
2020-07-31 15:35:17 -0400

    Create about.js

---
+++ fitness/src/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About(){
+    return(
+        <div>
+            <p>We wanted to create and app that blah blah blah</p>
+            <p>maybe include some info about the devs...</p>
+        </div>
+    )
+}
